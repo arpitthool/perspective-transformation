e9b017c120083498d514eb77146bc3c53aa43302
Arpit Thool <arpitthool@vt.edu>
2025-10-04 23:49:15 -0400

    - added option to draw 4 points on the video frame to define our viewing window plane
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 using namespace cv;
+bool pointSelected[4] = {false, false, false, false};
+Point points[4] = {Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0)};
+Mat frame;
 
 void showMenu(string windowName) {
     // show the menu
@@ -14,7 +17,10 @@ void showMenu(string windowName) {
     // we use CV_8UC3 because we want to display the text in color
     Mat menuImage = Mat::zeros(300, 300, CV_8UC3);
     vector<string> menuItems = {
-        "Press ESC or Q to quit"
+        "Press ESC or Q to quit",
+        "Press T to click and select 4",
+        "points on the image to get the ",
+        "perspective transformation"
     };  
 
     int menuItemsSize = menuItems.size();
@@ -25,6 +31,39 @@ void showMenu(string windowName) {
     imshow(windowName, menuImage);
 }
 
+void mouseCallback(int event, int x, int y, int flags, void* userdata) {
+    // Debug: Print all mouse events
+    if (event == EVENT_MOUSEMOVE) {
+        // Only print every 10th mouse move to avoid spam
+        static int moveCount = 0;
+        if (++moveCount % 10 == 0) {
+            cout << "Mouse moved to (" << x << ", " << y << ")" << endl;
+        }
+    }
+    
+    if (event == EVENT_LBUTTONDOWN) {
+        if (!pointSelected[0]) {
+            points[0] = Point(x, y);
+            pointSelected[0] = true;
+            cout << "First point selected: " << points[0] << endl;
+        } else if (!pointSelected[1]) {
+            points[1] = Point(x, y);
+            pointSelected[1] = true;
+            cout << "Second point selected: " << points[1] << endl;
+        } else if (!pointSelected[2]) {
+            points[2] = Point(x, y);
+            pointSelected[2] = true;
+            cout << "Third point selected: " << points[2] << endl;
+        } else if (!pointSelected[3]) {
+            points[3] = Point(x, y);
+            pointSelected[3] = true;
+            cout << "Fourth point selected: " << points[3] << endl;
+        }
+    }
+}
+
+
+
 int main() {
     string windowName = "Perspective Transformation", menuWindowName = "Menu";
 
@@ -34,19 +73,38 @@ int main() {
         cout << "Error: Could not open video file" << endl;
         return -1;
     }
-
-    Mat frame;
+    
+    // Create the window first
+    namedWindow(windowName, WINDOW_AUTOSIZE);
+    namedWindow(menuWindowName, WINDOW_AUTOSIZE);
+    
+    // Then set the mouse callback
+    setMouseCallback(windowName, mouseCallback, NULL);
+    cout << "Mouse callback set for window: " << windowName << endl;
     
     while (true) {
         video >> frame;
         
         if (frame.empty()) break;
+        for (int i = 0; i < 4; i++) {
+            if (pointSelected[i]) {
+                circle(frame, points[i], 5, Scalar(0, 0, 255), -1);
+            }
+        }
         
+
         imshow(windowName, frame);
         showMenu(menuWindowName);
 
         int key = waitKey(30);
-        if (key == 27 || key == 'q') break;
+        if (key == 27 || key == 'q' || key == 'Q') {
+            break;
+        } else if (key == 't' || key == 'T') {
+            cout << "T key pressed. Click on 4 points in the video window." << endl;
+            for (int i = 0; i < 4; i++) {
+                pointSelected[i] = false;
+            }
+        }
     }
     
     return 0;
